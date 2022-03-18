ed6a2ac12c52f7c81f67bd0d339d89bd15990fbc
rohanwahul <rohanwahul@gmail.com>
2022-03-18 23:30:38 +0530

    new

---
+++ 3.2contructor.eg.cs
@@ -0,0 +1,33 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C.netpractice
+{
+    class Student
+    {
+        private int Rolln;
+        private string Sname;
+
+        public Student(int rno,string name)
+        {
+            Rolln = rno;
+            Sname = name;
+
+        }
+
+        public string Studata()
+        {
+
+            return $"Student name : {Rolln}  Roll Number : {Sname}";
+        }
+        static void Main(string[] args)
+        {
+            Student mystu = new Student(5,"santosh");
+            Console.WriteLine(mystu.Studata());
+        }
+
+    }
+}*/
