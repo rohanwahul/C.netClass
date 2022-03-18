2a8f27fcf2cc0093b91f55a314434e20ccc3947c
rohanwahul <rohanwahul@gmail.com>
2022-03-18 20:45:20 +0530

    method

---
+++ method.cs
@@ -0,0 +1,37 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C.netpractice
+{
+    class Dept
+    {
+        private int id;
+        private string name;
+         private string loc;
+
+        public void AsignData()
+        {
+            id = 1;
+            name = "Rohan";
+            loc = "pune";
+
+        }
+        public string GetData()
+        {
+            return $"Department id : {id} NAME : {name} Location : {loc}";
+
+        }
+        static void Main(string[] args)
+        {
+
+            Dept emp = new Dept();
+            emp.AsignData();
+            Console.WriteLine(emp.GetData());
+
+        }
+    }
+
+}*/
