fb20ad34bf9bdd6bbfc678d1c4be60a48cd6df3b
rohanwahul <rohanwahul@gmail.com>
2022-03-22 20:31:29 +0530

    new

---
+++ 9.outKeyword.cs
@@ -0,0 +1,23 @@
+﻿/*using System;
+namespace C.netpractice
+{
+    class Class2
+
+    {
+        static void Cal(int a,int b ,out int add,out int sub)
+        {
+
+            add = a + b;
+            sub = a - b;
+        }
+        static void Main(string[] args)
+        {
+            int a = 4, b = 2, add, sub;
+            Cal(a, b, out add, out sub);
+            Console.WriteLine(add);
+            Console.WriteLine(sub);  // byusing out key word we can return multiple values
+
+        }
+    }
+
+}*/
---
+++ 9.property_get_set.cs
@@ -0,0 +1,35 @@
+﻿/*using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnetdemo1
+{
+    public class Dept
+    {
+        private int id;
+        private string dname;
+        public int Id
+        {
+            set { id = value; }
+            get { return id; }
+        }
+        public String Dname
+        {
+            set { dname = value; }
+            get { return dname; }
+        }
+    }
+
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Dept d1 = new Dept();
+            d1.Id = 1;
+            d1.Dname = "ron";
+            Console.WriteLine(d1.Id + d1.Dname);
+        }
+    }
+}*/
