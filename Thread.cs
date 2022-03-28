32bd4f43d45bd4db4950fdd62ba764655aeab7d8
rohanwahul <rohanwahul@gmail.com>
2022-03-28 23:38:37 +0530

    new

---
+++ Thread.cs
@@ -0,0 +1,45 @@
+﻿/*using System;
+using System.Threading;
+
+namespace C.netpractice
+{
+    class ThreadDemo
+    {
+        public void Test()
+        {   
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+
+            }
+        }
+
+        public void Test2()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Hello");
+
+            }
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadDemo Th = new ThreadDemo();
+            //using thred cls we created thread t1
+            Thread t1 = new Thread(new ThreadStart(Th.Test));
+            Thread t2 = new Thread(new ThreadStart(Th.Test2));
+            t1.Start();
+            t2.Start();
+
+        }
+
+
+
+    }
+}*/
