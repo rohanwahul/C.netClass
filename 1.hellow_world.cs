c967e723f0d7ef23ecbd0bb8d1c520c5666f9a9f
rohanwahul <rohanwahul@gmail.com>
2022-04-08 10:43:56 -0700

    new

--- 1.hellow_world.cs
+++ 1.hellow_world.cs
@@ -11,7 +11,7 @@ namespace C.netpractice
         static void Main(string[] args)
         {
 
-            Console.WriteLine("hellow world");
+            Console.WriteLine("hellow . world");
         }
     }
 }*/
